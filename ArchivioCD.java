f2315d7584735936293663cbc01b997103b8643a
claduio <el.polipao@gmail.com>
2015-05-05 16:47:45 +0200

    Create ArchivioCD.java

---
+++ ArchivioCD.java
@@ -0,0 +1,37 @@
+package unibs.ing.aut.java.claduio;
+
+import java.util.ArrayList;
+
+public class ArchivioCD {
+
+	ArrayList<CD> elencoCD = new ArrayList<CD>();
+	
+	public ArchivioCD(CD... nuovoCD){
+		
+		elencoCD.add(nuovoCD);
+		
+	}
+	
+	
+	public void addCD (CD nuovo){
+		
+	}
+	
+	public CD searchCD(CD nuovo){
+		
+	}
+	
+	public CD displayCD(CD nuovo){
+		
+	}
+	
+	public void deleteCD(CD nuovo){
+		
+	}
+	
+	public CD randomCD(CD nuovo){
+		
+	}
+
+	
+}
