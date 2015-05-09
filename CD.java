62b7511b475ebf9f46aca691376adb50b4c593c4
Bond1995 <m.bondaschi@studenti.unibs.it>
2015-05-09 18:22:48 +0200

    Update CD.java

--- CD.java
+++ CD.java
@@ -1,42 +1,75 @@
-import java.util.ArrayList;
 
-public class CD {
+import java.util.ArrayList;
+import ing.unibs.esercitazioni.mylib.*;;
+
+public class Cd {
+	
 	private String titolo;
 	private String autore;
-	ArrayList<Brano> listaBrani = new ArrayList<Brano>();
+	private ArrayList<Brano> listaBrani;
+	public static final String MESS_LISTA_BRANI = "\nLista Brani:\n";
+	public static final String MESS_ELENCO_BRANI = "%d - %s";
 	
-	public CD (String titolo, String autore){
+	public Cd (String titolo, String autore){
+		
 		this.titolo = titolo;
 		this.autore = autore;
+		listaBrani = new ArrayList<Brano>();
+		
 	}
 	
-	public void addBrano(Brano brano){
+	public void aggiungiBrano(Brano brano){
 		
 		listaBrani.add(brano);
 	}
 	
-	public Brano estraiBrano(){
+	public Brano branoCasuale(){
 		
-		return listaBrani.get(randomInt(0, listaBrani.size()-1));
+		return listaBrani.get(myUtil.randomInt(0, listaBrani.size() - 1));
+		
 	}
 	
 	public Brano ricercaBrano(int i){
 		return listaBrani.get(i);
 	}
 
+	public boolean equalsCD(String autore, String titolo) {
+		
+		if (this.autore.equalsIgnoreCase(autore) && this.titolo.equalsIgnoreCase(titolo)) return true;
+		return false;
+		
+	}
+	
+	public String getTitolo() {
+		
+		return this.titolo;
+		
+	}
+	
+	public boolean haTitolo(String titolo) {
+		
+		if (this.titolo == titolo) return true;
+		return false;
+		
+	}
+	
 	@Override
 	public String toString() {
+		
 		StringBuilder builder = new StringBuilder();
-		builder.append("Autore: ");
-		builder.append(autore + ";");
-		builder.append("Titolo: ");
-		builder.append(titolo + ";");
-		for(Brano b: listaBrani){
-			b.toString();
+		builder.append("Autore: ").append(autore).append(" - Titolo: ").append(titolo);
+		
+		if (listaBrani.size() > 0) builder.append(MESS_LISTA_BRANI);
+		
+		for (int i = 0; i < listaBrani.size(); i++) {
+			
+			Brano elemento = listaBrani.get(i);
+			builder.append(String.format(MESS_ELENCO_BRANI, (i+1), elemento.toString()));
+			
 		}
+		
 		return builder.toString();
+		
 	}
-
-	
 	
 }
