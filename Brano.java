ff435961b4840c65d4b1847205a1abb9e110896c
Bond1995 <m.bondaschi@studenti.unibs.it>
2015-05-09 18:21:48 +0200

    Update Brano.java

--- Brano.java
+++ Brano.java
@@ -1,26 +1,33 @@
-package unibs.ing.aut.java.claduio;
 
 public class Brano {
+
+	private String titolo;
+	private int minuti;
+	private int secondi;
+
+	public Brano(String titolo, int minuti, int secondi){
+
+		this.titolo = titolo;
+		this.minuti = minuti;
+		this.secondi = secondi;
+		
+	}
+
+	public String getTitolo() {
+		
+		return titolo;
+		
+	}
 	
-private String titolo;
-private String durata;
-
-public Brano(String titolo, String durata){
-	
-	this.titolo = titolo;
-	this.durata= durata;
-}
-
-@Override
-public String toString() {
-	StringBuilder builder = new StringBuilder();
-	builder.append("Titolo: ");
-	builder.append(titolo + ";" );
-	builder.append("durata: ");
-	builder.append(durata + ".\n");
-
-	return builder.toString();
-}
-
+	@Override
+	public String toString() {
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("Titolo: ").append(titolo).append(" - Durata: ");
+		
+		builder.append(minuti).append(" min ").append(secondi).append(" s;\n");
+		return builder.toString();
+		
+	}
 
 }
